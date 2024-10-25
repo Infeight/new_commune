25585d1430aa4f8981a458ba5a68ffe12fe4bbea
Tarund777 <tarundandugula1@gmail.com>
2024-10-25 07:05:08 +0530

    corsaddrupdate

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const port = 5004;
 
 // app.use(cors())
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://commune1-1.onrender.com/');  // or '*'
+  res.header('Access-Control-Allow-Origin', '*');  // or '*'
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
@@ -517,7 +517,7 @@ res.json({
 
   const io = socket(server,{
     cors:{
-      origin:'https://commune1-1.onrender.com/',
+      origin:'https://commune1-1.onrender.com',
       Credential:true
     }
   })
